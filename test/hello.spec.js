a0937863dc1027ca399dff726bafc6d36cb6f589
Supasate Choochaisri <supasate.c@gmail.com>
2016-10-26 19:23:57 +0700

    Rename to hello

--- test/hello.spec.js
+++ test/hello.spec.js
@@ -1,5 +1,5 @@
 const expect = require('chai').expect
-const hello = require('../src/')
+const hello = require('../src/hello')
 
 describe('hello', () => {
   it('should return a string with hello with supplied argument', () => {
